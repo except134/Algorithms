09320b1fa0c273e45385f82dac050f37f672a922
except <except134@gmail.com>
2021-08-03 08:18:44 +0300

    Lesson2

---
+++ Lesson2/cmake-build-debug/CMakeFiles/Lesson2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lesson2.dir/main.cpp.o"
+  "Lesson2"
+  "Lesson2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lesson2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Lesson2/main.cpp
+++ Lesson2/main.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+bool IsPrime(int val)
+{
+    int finish = sqrt(abs(val));
+    for (int i = 2; i <= finish; i++) {
+        if (val % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int num = 0;
+
+    printf("Введите число: ");
+
+    scanf("%d", &num);
+
+    printf("Число %d %sпростое\n",num,IsPrime(num)?"":"не ");
+
     return 0;
 }
