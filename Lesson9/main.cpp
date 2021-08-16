c67e7ff18172fd47fde2ea69e221ff5d426afe69
except134 <except134@gmail.com>
2021-08-16 13:23:53 +0300

    Added lesson 9

---
+++ Lesson9/main.cpp
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+///////////////
+// Задача 1
+
+void Task1()
+{
+}
+
+int main()
+{
+    Task1();
+
+    return 0;
+}
