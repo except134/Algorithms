1720394950c03a59b7a6c853bff25a1ce12bbf55
except134 <except134@gmail.com>
2021-08-23 10:19:30 +0300

    Added lesson 12

---
+++ Lesson12/main.cpp
@@ -0,0 +1,28 @@
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+
+///////////////
+// Задача 1
+
+void Task1()
+{
+    std::cout << "Задача 1." << std::endl;
+}
+
+///////////////
+// Задача 2
+
+void Task2()
+{
+    std::cout << std::endl << "Задача 2." << std::endl;
+}
+
+int main()
+{
+    Task1();
+    Task2();
+
+    return 0;
+}
