4bcfa7b4c7b110f26776e94e3233c7a99477d101
except134 <except134@gmail.com>
2021-08-23 14:49:36 +0300

    Added lesson 12

--- Lesson12/main.cpp
+++ Lesson12/main.cpp
@@ -3,6 +3,71 @@
 #include <iostream>
 #include <cstring>
 
+class Node
+{
+public:
+    Node() = default;
+    Node(int k) : key(k) {}
+
+    void GetHeight()
+    {
+        key = 0;
+        if (left != nullptr) {
+            left->GetHeight();
+            key = std::max(key, left->key + 1);
+        }
+        if (right != nullptr) {
+            right->GetHeight();
+            key = std::max(key, right->key + 1);
+        }
+    }
+    
+public:
+    int key = 0;
+    Node* left{nullptr};
+    Node* right{nullptr};
+};
+
+class Tree
+{
+public:
+    Tree() = default;
+
+    ~Tree()
+    {
+        DestroyNode(root);
+    }
+
+    void Insert(int x)
+    {
+        Node** cur = &root;
+        while (*cur) {
+            Node& node = **cur;
+            if (x < node.key) {
+                cur = &node.left;
+            } else if (x > node.key) {
+                cur = &node.right;
+            } else {
+                return;
+            }
+        }
+        *cur = new Node(x);
+    }
+
+private:
+    static void DestroyNode(Node* node)
+    {
+        if(node) {
+            DestroyNode(node->left);
+            DestroyNode(node->right);
+            delete node;
+        }
+    }
+
+private:
+    Node* root{nullptr};
+};
+
 ///////////////
 // Задача 1
 
