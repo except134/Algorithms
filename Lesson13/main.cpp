fb12dd14e7012013e03584f82da44beeaffa89ff
except <except134@gmail.com>
2021-08-26 19:35:54 +0300

    Added lesson 13

--- Lesson13/main.cpp
+++ Lesson13/main.cpp
@@ -50,6 +50,13 @@ public:
         return data[count];
     }
 
+    int Peek()
+    {
+        if (count == 0)
+            return 0;
+        return data[count - 1];
+    }
+
     void Print()
     {
         int* temp = data;
@@ -67,12 +74,72 @@ private:
     int count = 0;
 };
 
+class Graf
+{
+public:
+    Graf() = default;
+
+    ~Graf()
+    {
+        for(int index = 0; index < count; ++index) {
+            delete[] graf[index];
+        }
+        delete[] graf;
+    }
+
+    void Init(int n, int m)
+    {
+        count = n;
+
+        if(m > n * (n - 1) / 2) {
+            std::cout << "Слишком много граней." << std::endl;
+            return;
+        }
+
+        graf = new int*[n];
+        for(int index = 0; index < n; ++index) {
+            graf[index] = new int[n];
+        }
+
+        while(m) {
+            for(int i = 0; i < n && m; ++i) {
+                for(int j = i + 1; j < n && m; ++j) {
+                    int v = GetRandomInt(0,10) > 3 ? 1 : 0;
+                    if (v && !graf[i][j]) {
+                        graf[i][j] = graf[j][i] = v;
+                        m--;
+                    }
+                }
+            }
+        }
+    }
+
+    void Print()
+    {
+        for(int i = 0; i < count; ++i) {
+            for(int j = 0; j < count; ++j) {
+                std::cout << graf[i][j];
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+    }
+
+private:
+    int** graf{nullptr};
+    int count=0;
+};
+
 ///////////////
 // Задача 1
 
 void Task1()
 {
     std::cout << "Задача 1." << std::endl;
+
+    Graf graf;
+    graf.Init(5, 7);
+    graf.Print();
 }
 
 ///////////////
